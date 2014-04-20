9cde2b4d3c1c30a2fb2268e4025ec4f66bf02cdb
jeff994 <hujianbo1981@gmai.com>
2014-04-20 10:07:57 +0800

    Added web view handle first version for sgtips

---
+++ SGExpense/DealViewController.h
@@ -0,0 +1,14 @@
+//
+//  DealViewController.h
+//  SGExpense
+//
+//  Created by Hu Jianbo on 20/4/14.
+//  Copyright (c) 2014 SD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DealViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIWebView *DealView;
+
+@end
---
+++ SGExpense/DealViewController.m
@@ -0,0 +1,55 @@
+//
+//  DealViewController.m
+//  SGExpense
+//
+//  Created by Hu Jianbo on 20/4/14.
+//  Copyright (c) 2014 SD. All rights reserved.
+//
+
+#import "DealViewController.h"
+
+@interface DealViewController ()
+//@property (strong, nonatomic) IBOutlet UIWebView *pUIWebView;
+
+@end
+
+@implementation DealViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setTitle:@"Deals"];
+    NSString *httpSource = @"http://www.sgtips.com/deal";
+    NSURL *fullUrl = [NSURL URLWithString:httpSource];
+    NSURLRequest *httpRequest = [NSURLRequest requestWithURL:fullUrl];
+    [self.DealView loadRequest:httpRequest];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
